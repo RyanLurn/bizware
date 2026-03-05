4c1fb44e16460a99ddf0f9c3323e18d0ab29e0c0
Ryan Luong <lumintriet@gmail.com>
2026-03-05 21:40:18 +0700

    feat(ui/theme): add util function to save preference to localStorage.

--- packages/ui/src/theme/storage.ts
+++ packages/ui/src/theme/storage.ts
@@ -1,6 +1,44 @@
+import type { Failure, Success } from "@bizware/common/types/result";
+import type { FallbackError } from "@bizware/error";
+
+import { createFallbackError, BrowserOnlyError } from "@bizware/error";
+
+import type { UserTheme } from "@/theme/schemas";
+
 import { UserThemeSchema } from "@/theme/schemas";
 
 export const THEME_STORAGE_KEY = "ui-theme";
+
+export function setStoredTheme({ theme }: { theme: UserTheme }) {
+  if (typeof window === "undefined") {
+    const error = new BrowserOnlyError({
+      context: { apis: ["localStorage.setItem()"], feature: "setStoredTheme" },
+    });
+
+    const failure: Failure<BrowserOnlyError> = { success: false, error };
+    return failure;
+  }
+
+  try {
+    const validTheme = UserThemeSchema.parse(theme);
+    localStorage.setItem(THEME_STORAGE_KEY, validTheme);
+
+    const success: Success<undefined> = { data: undefined, success: true };
+    return success;
+  } catch (error) {
+    const fallbackError = createFallbackError(
+      "Failed to set theme in local storage",
+      {
+        cause: error,
+      }
+    );
+    const failure: Failure<FallbackError> = {
+      error: fallbackError,
+      success: false,
+    };
+    return failure;
+  }
+}
 
 export function getStoredTheme() {
   if (typeof window === "undefined") {
