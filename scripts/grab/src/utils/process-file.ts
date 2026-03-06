440ecdda51c37d853c14306dd2a8977fe0ddd374
Ryan Luong <lumintriet@gmail.com>
2026-03-06 19:22:48 +0700

    feat(grab): add glob pattern to ignore drizzle-kit migration metadata.

--- scripts/grab/src/utils/process-file.ts
+++ scripts/grab/src/utils/process-file.ts
@@ -1,16 +1,23 @@
 import { parse } from "path";
+import { Glob } from "bun";
 
 import { formatOutput } from "@/utils/format-output";
 
 const ignoredBases = ["routeTree.gen.ts"];
 const ignoredExts = [".lock", ".svg"];
 
+const migrationMetadataGlob = new Glob("**/migrations/meta/*.json");
+
 export async function processFile({ path }: { path: string }) {
   const parsedPath = parse(path);
   const ext = parsedPath.ext;
 
   let content: string;
-  if (ignoredExts.includes(ext) || ignoredBases.includes(parsedPath.base)) {
+  if (
+    ignoredExts.includes(ext) ||
+    ignoredBases.includes(parsedPath.base) ||
+    migrationMetadataGlob.match(path)
+  ) {
     content = "[SKIPPED]";
   } else {
     content = await Bun.file(path).text();
