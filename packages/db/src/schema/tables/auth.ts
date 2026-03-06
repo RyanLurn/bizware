3a39e3dc45ba797d8feff9739b77f1ca52894e0e
Ryan Luong <lumintriet@gmail.com>
2026-03-06 18:36:48 +0700

    feat(db/schema): define verifications table schema.

--- packages/db/src/schema/tables/auth.ts
+++ packages/db/src/schema/tables/auth.ts
@@ -73,3 +73,19 @@ export const accountTable = pgTable(
   },
   (table) => [index("accounts_user_id_idx").on(table.userId)]
 );
+
+export const verificationTable = pgTable(
+  "verifications",
+  {
+    id,
+    identifier: text("identifier").notNull(),
+    value: text("value").notNull(),
+    expiresAt: timestamp("expires_at", {
+      withTimezone: true,
+      mode: "date",
+      precision: 6,
+    }).notNull(),
+    ...timestamps,
+  },
+  (table) => [index("verifications_identifier_idx").on(table.identifier)]
+);
