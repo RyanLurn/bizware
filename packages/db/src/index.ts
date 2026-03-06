cefa74156147feb3116808a73969c18c64622f83
Ryan Luong <lumintriet@gmail.com>
2026-03-06 18:42:07 +0700

    feat(db): define db connection instance.

---
+++ packages/db/src/index.ts
@@ -0,0 +1,16 @@
+import { drizzle } from "drizzle-orm/neon-http";
+import { createEnv } from "@t3-oss/env-core";
+import { z } from "zod";
+
+import * as authSchema from "@/schema/tables/auth";
+
+const env = createEnv({
+  server: {
+    NEON_POOLED_CONNECTION_STRING: z.url(),
+  },
+  runtimeEnv: process.env,
+});
+
+export const db = drizzle(env.NEON_POOLED_CONNECTION_STRING, {
+  schema: { ...authSchema },
+});
