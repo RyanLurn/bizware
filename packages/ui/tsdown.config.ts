05c62a1ff6adc584338e000e47f0bb1222049b72
Ryan Luong <lumintriet@gmail.com>
2026-03-06 08:46:38 +0700

    chore(ui): configure tsdown with react compiler and bundle package.

--- packages/ui/tsdown.config.ts
+++ packages/ui/tsdown.config.ts
@@ -1,6 +1,18 @@
+import pluginBabel from "@rollup/plugin-babel";
 import { defineConfig } from "tsdown";
 
 export default defineConfig({
+  plugins: [
+    pluginBabel({
+      parserOpts: {
+        plugins: ["jsx", "typescript"],
+        sourceType: "module",
+      },
+      extensions: [".js", ".jsx", ".ts", ".tsx"],
+      plugins: ["babel-plugin-react-compiler"],
+      babelHelpers: "bundled",
+    }),
+  ],
   entry: {
     index: "./src/components/index.ts",
     theme: "./src/theme/index.ts",
