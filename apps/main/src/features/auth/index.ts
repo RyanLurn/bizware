3cab380c5d4ac949d24b36595f5ee25ab35eb5cb
Ryan Luong <lumintriet@gmail.com>
2026-03-07 08:57:09 +0700

    feat(auth): enable email password auth method.

--- apps/main/src/features/auth/index.ts
+++ apps/main/src/features/auth/index.ts
@@ -1,6 +1,7 @@
 import { tanstackStartCookies } from "better-auth/tanstack-start";
 import { betterAuth } from "better-auth";
 
+import { emailPasswordOptions } from "@/features/auth/configs/email-password";
 import { dbOptions } from "@/features/auth/configs/db";
 import { serverEnv } from "@/lib/env/server";
 
@@ -13,5 +14,6 @@ export const auth = betterAuth({
   secret: serverEnv.BETTER_AUTH_SECRET,
   baseURL: serverEnv.BETTER_AUTH_URL,
   ...dbOptions,
+  ...emailPasswordOptions,
   plugins: [tanstackStartCookies()], // TanStack Start cookie plugin must come last
 });
