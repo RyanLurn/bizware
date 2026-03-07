d19a1a2d549f8cc14c03f1f490f7292c9aef4785
Ryan Luong <lumintriet@gmail.com>
2026-03-08 00:54:57 +0700

    feat(main/auth): add validators for user logins.

--- apps/main/src/features/auth/configs/email-password.ts
+++ apps/main/src/features/auth/configs/email-password.ts
@@ -1,7 +1,14 @@
 import type { BetterAuthOptions } from "better-auth";
 
+import {
+  MAX_PASSWORD_LENGTH,
+  MIN_PASSWORD_LENGTH,
+} from "@/features/auth/constants";
+
 export const emailPasswordOptions: BetterAuthOptions = {
   emailAndPassword: {
+    maxPasswordLength: MAX_PASSWORD_LENGTH,
+    minPasswordLength: MIN_PASSWORD_LENGTH,
     enabled: true,
   },
 };
---
+++ apps/main/src/features/auth/validators.ts
@@ -0,0 +1,16 @@
+import { z } from "zod";
+
+import {
+  MIN_PASSWORD_LENGTH,
+  MAX_PASSWORD_LENGTH,
+} from "@/features/auth/constants";
+
+// For context, the longest name in Vietnam is only 43 characters long
+export const NameSchema = z.string().min(1).max(150);
+
+export const EmailSchema = z.email({ pattern: z.regexes.unicodeEmail });
+
+export const PasswordSchema = z
+  .string()
+  .min(MIN_PASSWORD_LENGTH)
+  .max(MAX_PASSWORD_LENGTH);
