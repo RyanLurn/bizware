fdecf1b83664ad45f8a6dc903d80b8c10874e5ad
Ryan Luong <lumintriet@gmail.com>
2026-03-07 09:25:57 +0700

    feat(ui/form): export useFormContext hook.

--- packages/ui/src/features/form/context.ts
+++ packages/ui/src/features/form/context.ts
@@ -1,4 +1,4 @@
 import { createFormHookContexts } from "@tanstack/react-form";
 
-export const { useFieldContext, fieldContext, formContext } =
+export const { useFieldContext, useFormContext, fieldContext, formContext } =
   createFormHookContexts();
