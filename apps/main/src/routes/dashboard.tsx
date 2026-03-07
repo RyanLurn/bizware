49915c5dc2c606b525ee40bbd5ff9747f7b94b70
Ryan Luong <lumintriet@gmail.com>
2026-03-07 08:50:09 +0700

    feat(main): add protection to dashboard route.

--- apps/main/src/routes/dashboard.tsx
+++ apps/main/src/routes/dashboard.tsx
@@ -1,9 +1,17 @@
 import { createFileRoute } from "@tanstack/react-router";
 
+import { getUser } from "@/lib/server-functions/get-user";
+
 export const Route = createFileRoute("/dashboard")({
+  beforeLoad: async () => {
+    const user = await getUser();
+    return { user };
+  },
   component: Dashboard,
 });
 
 function Dashboard() {
-  return <div>Hello "/dashboard"!</div>;
+  const { user } = Route.useRouteContext();
+
+  return <div>Welcome, {user.name}!</div>;
 }
