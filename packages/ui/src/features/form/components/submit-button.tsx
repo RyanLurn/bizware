3fad7b1e10214eeaa32bb0db1a8630921ac3d317
Ryan Luong <lumintriet@gmail.com>
2026-03-07 10:14:28 +0700

    feat(ui/form): define SubmitButton component.

---
+++ packages/ui/src/features/form/components/submit-button.tsx
@@ -0,0 +1,48 @@
+import type { ComponentProps } from "react";
+
+import { useFormContext } from "@/features/form/context";
+import { Spinner, Button } from "@/components";
+
+interface SubmitButtonProps extends Omit<
+  ComponentProps<typeof Button>,
+  "disabled" | "type" | "form"
+> {
+  submittingText: string;
+  submitText: string;
+}
+
+export function SubmitButton({
+  submittingText,
+  submitText,
+  ...props
+}: SubmitButtonProps) {
+  const form = useFormContext();
+
+  return (
+    <form.Subscribe
+      selector={(state) => ({
+        isSubmitting: state.isSubmitting,
+        isPristine: state.isPristine,
+        canSubmit: state.canSubmit,
+      })}
+    >
+      {({ isSubmitting, isPristine, canSubmit }) => (
+        <Button
+          disabled={!canSubmit || isSubmitting || isPristine}
+          form={form.formId}
+          type="submit"
+          {...props}
+        >
+          {isSubmitting ? (
+            <>
+              <Spinner />
+              {submittingText}
+            </>
+          ) : (
+            submitText
+          )}
+        </Button>
+      )}
+    </form.Subscribe>
+  );
+}
