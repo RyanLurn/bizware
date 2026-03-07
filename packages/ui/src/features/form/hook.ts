64fa07fce7e392d83fc59cdff91d39bbfc4875cc
Ryan Luong <lumintriet@gmail.com>
2026-03-07 10:17:04 +0700

    feat(ui/form): add TextInput and SubmitButton components to useAppForm hook.

--- packages/ui/src/features/form/hook.ts
+++ packages/ui/src/features/form/hook.ts
@@ -1,10 +1,12 @@
 import { createFormHook } from "@tanstack/react-form";
 
+import { SubmitButton } from "@/features/form/components/submit-button";
 import { fieldContext, formContext } from "@/features/form/context";
+import { TextInput } from "@/features/form/components/text-input";
 
 export const { useAppForm } = createFormHook({
+  formComponents: { SubmitButton, TextInput },
   fieldComponents: {},
-  formComponents: {},
   fieldContext,
   formContext,
 });
