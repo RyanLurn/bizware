0e0aa891bc9be50b13e3727b919b170d9f64b446
Ryan Luong <lumintriet@gmail.com>
2026-03-08 10:29:56 +0700

    feat(main/auth/ui): add content to verify email page.

--- apps/main/src/routes/(auth)/verify-email.tsx
+++ apps/main/src/routes/(auth)/verify-email.tsx
@@ -5,5 +5,16 @@ export const Route = createFileRoute("/(auth)/verify-email")({
 });
 
 function VerifyEmail() {
-  return <div>Hello "/(auth)/verify-email"!</div>;
+  return (
+    <div className="flex h-dvh w-full flex-col items-center justify-center gap-4 p-6 md:p-10">
+      <h1 className="text-3xl font-bold">Hãy xác thực email của bạn</h1>
+      <p className="text-muted-foreground">
+        Chúng tôi đã gửi một email xác thực đến địa chỉ email của bạn.
+      </p>
+      <p className="text-muted-foreground">
+        Vui lòng kiểm tra hộp thư và nhấp vào liên kết xác thực để hoàn tất quá
+        trình đăng ký.
+      </p>
+    </div>
+  );
 }
