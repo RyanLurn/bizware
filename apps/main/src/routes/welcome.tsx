0a2f0edcf48492c88611e6f4718bab0287dc1466
Ryan Luong <lumintriet@gmail.com>
2026-03-09 09:25:55 +0700

    feat(main/onboard/ui): make welcome route shows different page content whether the token is valid or not.

--- apps/main/src/routes/welcome.tsx
+++ apps/main/src/routes/welcome.tsx
@@ -1,9 +1,30 @@
 import { createFileRoute } from "@tanstack/react-router";
+import { z } from "zod";
+
+import { InvalidTokenMessage } from "@/features/auth/components/invalid-token-message";
 
 export const Route = createFileRoute("/welcome")({
+  validateSearch: z.object({
+    error: z.string().optional(),
+  }),
   component: Welcome,
 });
 
 function Welcome() {
-  return <div>Hello "/welcome"!</div>;
+  const searchParams = Route.useSearch();
+
+  return (
+    <div className="flex h-dvh w-full flex-col items-center justify-center gap-y-6 p-6 md:p-10">
+      {"error" in searchParams ? (
+        <InvalidTokenMessage />
+      ) : (
+        <>
+          <h1 className="text-3xl font-bold">Chào mừng!</h1>
+          <div className="flex flex-col items-center justify-center gap-1 text-muted-foreground">
+            <p>Cảm ơn bạn đã đăng ký tài khoản.</p>
+          </div>
+        </>
+      )}
+    </div>
+  );
 }
