ed1db1a872aa37ac2f35aa283ddb31340b9b11ac
Ryan Luong <lumintriet@gmail.com>
2026-03-09 07:48:32 +0700

    refactor(main/auth): rename reset password route component to ResetPassword.

--- apps/main/src/routes/(auth)/reset-password.tsx
+++ apps/main/src/routes/(auth)/reset-password.tsx
@@ -1,9 +1,9 @@
 import { createFileRoute } from "@tanstack/react-router";
 
 export const Route = createFileRoute("/(auth)/reset-password")({
-  component: RouteComponent,
+  component: ResetPassword,
 });
 
-function RouteComponent() {
+function ResetPassword() {
   return <div>Hello "/(auth)/reset-password"!</div>;
 }
