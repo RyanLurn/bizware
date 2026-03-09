78df082714703b05f57b2ebbfabfa074505e5807
Ryan Luong <lumintriet@gmail.com>
2026-03-09 08:10:36 +0700

    feat(main/auth/ui): implement conditional UI based in the reset password route based on whether the token is valid or not.

--- apps/main/src/routes/(auth)/reset-password.tsx
+++ apps/main/src/routes/(auth)/reset-password.tsx
@@ -1,5 +1,6 @@
 import { createFileRoute } from "@tanstack/react-router";
 
+import { ResetPasswordForm } from "@/features/auth/components/forms/reset-password";
 import { VerificationRouteSearchParamsSchema } from "@/features/auth/validators";
 
 export const Route = createFileRoute("/(auth)/reset-password")({
@@ -8,5 +9,15 @@ export const Route = createFileRoute("/(auth)/reset-password")({
 });
 
 function ResetPassword() {
-  return <div>Hello "/(auth)/reset-password"!</div>;
+  const searchParams = Route.useSearch();
+
+  return (
+    <div className="flex h-dvh w-full items-center justify-center p-6 md:p-10">
+      {"token" in searchParams ? (
+        <ResetPasswordForm token={searchParams.token} />
+      ) : (
+        <div>Invalid token</div>
+      )}
+    </div>
+  );
 }
