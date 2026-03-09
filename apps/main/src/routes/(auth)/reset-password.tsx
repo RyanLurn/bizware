6bf597a6d7a72fe582f11f123e81cafc7ffcca71
Ryan Luong <lumintriet@gmail.com>
2026-03-09 08:15:49 +0700

    feat(main/auth/ui): add proper content to reset password page when the token is invalid.

--- apps/main/src/routes/(auth)/reset-password.tsx
+++ apps/main/src/routes/(auth)/reset-password.tsx
@@ -12,11 +12,17 @@ function ResetPassword() {
   const searchParams = Route.useSearch();
 
   return (
-    <div className="flex h-dvh w-full items-center justify-center p-6 md:p-10">
+    <div className="flex h-dvh w-full flex-col items-center justify-center gap-y-6 p-6 md:p-10">
       {"token" in searchParams ? (
         <ResetPasswordForm token={searchParams.token} />
       ) : (
-        <div>Invalid token</div>
+        <>
+          <h1 className="text-3xl font-bold">Liên kết xác thực không hợp lệ</h1>
+          <div className="flex flex-col items-center justify-center gap-1 text-muted-foreground">
+            <p>Liên kết xác thực không hợp lệ hoặc đã hết hạn.</p>
+            <p>Vui lòng kiểm tra lại email hoặc yêu cầu liên kết mới.</p>
+          </div>
+        </>
       )}
     </div>
   );
