df17d9ddcef194a53612b52f2c445b628e2ee347
Ryan Luong <lumintriet@gmail.com>
2026-03-09 09:36:22 +0700

    feat(main/onboard/ui): redirect to invalid token route if the token is invalid; say welcome to user with their name.

--- apps/main/src/routes/welcome.tsx
+++ apps/main/src/routes/welcome.tsx
@@ -1,9 +1,17 @@
-import { createFileRoute } from "@tanstack/react-router";
+import { createFileRoute, redirect } from "@tanstack/react-router";
 import { z } from "zod";
 
-import { InvalidTokenMessage } from "@/features/auth/components/invalid-token-message";
+import { getUser } from "@/lib/server-functions/get-user";
 
 export const Route = createFileRoute("/welcome")({
+  beforeLoad: async ({ search }) => {
+    if (Object.hasOwn(search, "error")) {
+      throw redirect({ to: "/invalid-token" });
+    }
+
+    const user = await getUser();
+    return { user };
+  },
   validateSearch: z.object({
     error: z.string().optional(),
   }),
@@ -11,20 +19,14 @@ export const Route = createFileRoute("/welcome")({
 });
 
 function Welcome() {
-  const searchParams = Route.useSearch();
+  const { user } = Route.useRouteContext();
 
   return (
     <div className="flex h-dvh w-full flex-col items-center justify-center gap-y-6 p-6 md:p-10">
-      {"error" in searchParams ? (
-        <InvalidTokenMessage />
-      ) : (
-        <>
-          <h1 className="text-3xl font-bold">Chào mừng!</h1>
-          <div className="flex flex-col items-center justify-center gap-1 text-muted-foreground">
-            <p>Cảm ơn bạn đã đăng ký tài khoản.</p>
-          </div>
-        </>
-      )}
+      <h1 className="text-3xl font-bold">Chào mừng, {user.name}!</h1>
+      <div className="flex flex-col items-center justify-center gap-1 text-muted-foreground">
+        <p>Cảm ơn bạn đã đăng ký tài khoản.</p>
+      </div>
     </div>
   );
 }
