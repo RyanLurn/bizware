e5a0fcbd9222ca6feaf7574acedde950dc6e335d
Ryan Luong <lumintriet@gmail.com>
2026-03-09 09:46:12 +0700

    feat(main/auth/ui): in reset password route, redirect user to invalid token route if the search params has error.

--- apps/main/src/routes/(auth)/reset-password.tsx
+++ apps/main/src/routes/(auth)/reset-password.tsx
@@ -1,24 +1,26 @@
-import { createFileRoute } from "@tanstack/react-router";
+import { createFileRoute, redirect } from "@tanstack/react-router";
 
-import { InvalidTokenMessage } from "@/features/auth/components/invalid-token-message";
 import { ResetPasswordForm } from "@/features/auth/components/forms/reset-password";
 import { VerificationRouteSearchParamsSchema } from "@/features/auth/validators";
 
 export const Route = createFileRoute("/(auth)/reset-password")({
+  beforeLoad: ({ search }) => {
+    if ("error" in search) {
+      throw redirect({ to: "/invalid-token" });
+    }
+
+    return { token: search.token };
+  },
   validateSearch: VerificationRouteSearchParamsSchema,
   component: ResetPassword,
 });
 
 function ResetPassword() {
-  const searchParams = Route.useSearch();
+  const { token } = Route.useRouteContext();
 
   return (
     <div className="flex h-dvh w-full flex-col items-center justify-center gap-y-6 p-6 md:p-10">
-      {"token" in searchParams ? (
-        <ResetPasswordForm token={searchParams.token} />
-      ) : (
-        <InvalidTokenMessage />
-      )}
+      <ResetPasswordForm token={token} />
     </div>
   );
 }
