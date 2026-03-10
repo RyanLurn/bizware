0f1493cc0d9afe78aef22ed1eae2b2ceef059c74
Ryan Luong <lumintriet@gmail.com>
2026-03-10 09:40:13 +0700

    feat(nickname): add adverb to the word construct.

--- packages/nickname/src/index.js
+++ packages/nickname/src/index.js
@@ -1,14 +1,16 @@
 import { firstNames } from "./datasets/first-names";
 import { adjectives } from "./datasets/adjectives";
 import { lastNames } from "./datasets/last-names";
+import { adverbs } from "./datasets/adverbs";
 import { nouns } from "./datasets/nouns";
 import { getRandomItem } from "./utils";
 
 export function generateNickname() {
   const randomFirstName = getRandomItem(firstNames);
   const randomLastName = getRandomItem(lastNames);
+  const randomAdverb = getRandomItem(adverbs);
   const randomAdjective = getRandomItem(adjectives);
   const randomNoun = getRandomItem(nouns);
 
-  return `${randomFirstName} ${randomLastName} the ${randomAdjective.charAt(0).toUpperCase() + randomAdjective.slice(1)} ${randomNoun.charAt(0).toUpperCase() + randomNoun.slice(1)}`;
+  return `${randomFirstName} ${randomLastName} the ${randomAdverb} ${randomAdjective.charAt(0).toUpperCase() + randomAdjective.slice(1)} ${randomNoun.charAt(0).toUpperCase() + randomNoun.slice(1)}`;
 }
