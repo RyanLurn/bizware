c953a6a9963ff0516d22bf5edffde9b8c8406d48
Ryan Luong <lumintriet@gmail.com>
2026-03-10 09:35:30 +0700

    feat(nickname): add gemstones dataset.

--- packages/nickname/src/datasets/nouns/index.js
+++ packages/nickname/src/datasets/nouns/index.js
@@ -1,6 +1,13 @@
 import { occupations } from "./occupations";
+import { gemstones } from "./gemstones";
 import { animals } from "./animals";
 import { flowers } from "./flowers";
 import { weapons } from "./weapons";
 
-export const nouns = [...animals, ...flowers, ...weapons, ...occupations];
+export const nouns = [
+  ...animals,
+  ...flowers,
+  ...weapons,
+  ...occupations,
+  ...gemstones,
+];
