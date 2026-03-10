187f7a6a4d1c68368cd8a3a77d49fffd9f424c80
Ryan Luong <lumintriet@gmail.com>
2026-03-10 09:27:48 +0700

    feat(nickname): add occupations dataset.

--- packages/nickname/src/datasets/nouns/index.js
+++ packages/nickname/src/datasets/nouns/index.js
@@ -1,5 +1,6 @@
+import { occupations } from "./occupations";
 import { animals } from "./animals";
 import { flowers } from "./flowers";
 import { weapons } from "./weapons";
 
-export const nouns = [...animals, ...flowers, ...weapons];
+export const nouns = [...animals, ...flowers, ...weapons, ...occupations];
