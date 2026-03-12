929bc15e6433fff3f4dd36d469077e34a5350cb5
Ryan Luong <lumintriet@gmail.com>
2026-03-12 16:40:39 +0700

    feat(common): replace the error throw with a comment.

--- packages/common/src/utils/get-random-item.js
+++ packages/common/src/utils/get-random-item.js
@@ -6,11 +6,6 @@
 export function getRandomItem(array) {
   const index = Math.floor(Math.random() * array.length);
   const item = array[index];
-
-  // Assertion to make TypeScript happy but it should never happen
-  if (item === undefined) {
-    throw new Error("Array must not be empty");
-  }
-
+  // @ts-expect-error - TypeScript can't infer the logic
   return item;
 }
