29797d07ae4eb903061cdcf92ddd4be0df060657
Ryan Luong <lumintriet@gmail.com>
2026-03-13 05:58:33 +0700

    feat(main): add auth check to app layout.

--- apps/main/src/routes/_app/route.tsx
+++ apps/main/src/routes/_app/route.tsx
@@ -1,6 +1,20 @@
-import { createFileRoute } from "@tanstack/react-router";
+import { createFileRoute, redirect } from "@tanstack/react-router";
+import { z } from "zod";
+
+import { getUser } from "@/lib/server-functions/get-user";
 
 export const Route = createFileRoute("/_app")({
+  beforeLoad: async ({ search }) => {
+    if ("error" in search) {
+      throw redirect({ to: "/invalid-token" });
+    }
+
+    const user = await getUser();
+    return { user };
+  },
+  validateSearch: z.object({
+    error: z.string().optional(),
+  }),
   component: AppLayout,
 });
 
