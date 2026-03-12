9110a786f649dfcae40079675b4de3b9c0e9f998
Ryan Luong <lumintriet@gmail.com>
2026-03-12 17:07:51 +0700

    feat(randomizer): add quantifiers dataset.

---
+++ packages/randomizer/src/datasets/quantifiers.ts
@@ -0,0 +1,16 @@
+export const exactQuantifiers = [
+  "mot",
+  "hai",
+  "ba",
+  "bon",
+  "nam",
+  "sau",
+  "bay",
+  "tam",
+  "chin",
+  "muoi",
+];
+
+export const indefiniteQuantifiers = ["vai", "nhieu", "cac", "nhung"];
+
+export const quantifiers = [...exactQuantifiers, ...indefiniteQuantifiers];
