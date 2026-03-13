5d78b24a9d5dacd6f187ce753e53d8e77230581d
Ryan Luong <lumintriet@gmail.com>
2026-03-13 08:05:42 +0700

    chore(ui): remove react compiler from build process and run build.

--- packages/ui/tsdown.config.ts
+++ packages/ui/tsdown.config.ts
@@ -1,18 +1,6 @@
-import pluginBabel from "@rollup/plugin-babel";
 import { defineConfig } from "tsdown";
 
 export default defineConfig({
-  plugins: [
-    pluginBabel({
-      parserOpts: {
-        plugins: ["jsx", "typescript"],
-        sourceType: "module",
-      },
-      extensions: [".js", ".jsx", ".ts", ".tsx"],
-      plugins: ["babel-plugin-react-compiler"],
-      babelHelpers: "bundled",
-    }),
-  ],
   entry: ["src/**/*.ts", "src/**/*.tsx"],
   dts: {
     sourcemap: true,
